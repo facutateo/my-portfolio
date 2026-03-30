ab3e8674e8307d94f1c36bb6b5babc17a853bebe
facutateo <facutateo@gmail.com>
2026-03-30 13:48:27 -0300

    published changes

--- src/components/boton-lenguage.tsx
+++ src/components/boton-lenguage.tsx
@@ -15,7 +15,7 @@ function BotonLenguage({ language, setLanguage }: BotonLenguageProps) {
         <label className="relative cursor-pointer hover:animate-squeeze hover:animate-iteration-count-infinite">
         <input type="checkbox" className="peer sr-only " checked={language === 'es'} onChange={toggleLenguage} />
             <div className="w-4 select-none">
-                <img src={language === 'es' ? '/SPANISH.svg' : '/ENGLISH.svg'} alt= {language === 'es' ? 'es' : 'en'}/>
+                <img src={language === 'es' ? 'SPANISH.svg' : 'ENGLISH.svg'} alt= {language === 'es' ? 'es' : 'en'}/>
             </div>
         </label>
     )
